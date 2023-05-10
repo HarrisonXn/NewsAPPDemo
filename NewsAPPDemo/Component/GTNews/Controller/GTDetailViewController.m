2cf468fcfa49655c8f12cf066896b87567429305
HarrisonXn <18236607607@163.com>
2023-05-10 14:09:15 +0800

    添加webView的进度条展示

--- NewsAPPDemo/Component/GTNews/Controller/GTDetailViewController.m
+++ NewsAPPDemo/Component/GTNews/Controller/GTDetailViewController.m
@@ -9,10 +9,13 @@
 
 @interface GTDetailViewController ()<WKNavigationDelegate>
 @property(nonatomic,strong,readwrite) WKWebView *webView;
+@property(nonatomic,strong) UIProgressView *progressView;
 @end
 
 @implementation GTDetailViewController
-
+- (void)dealloc{
+    [self.webView removeObserver:self forKeyPath:@"estimateProgress"];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -22,7 +25,15 @@
         self.webView;
     })];
     
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc]initWithFrame:CGRectMake(0, 88, self.view.bounds.size.width, 20)];
+        self.progressView;
+    })];
+    
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.toutiao.com/"]]];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    
 }
 
 #pragma mark -- webView相关delegate
@@ -32,5 +43,9 @@
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
     
+}
+#pragma mark -- KVO相关
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    self.progressView.progress = self.webView.estimatedProgress;
 }
 @end
